d077552f763ffc3be25a9bc5750183e0f7b1698e
Erez Volk <erez@neuralight.ai>
2025-04-19 12:52:52 +0300

    Fixed lock button on iOS

--- Droneroo/PlainButton.swift
+++ Droneroo/PlainButton.swift
@@ -4,9 +4,14 @@ import SwiftUI
 
 extension View {
     /// Make a button show only the image, without an outline
+
     func plainButton() -> some View {
+#if os(macOS)
         return buttonStyle(.plain)
             .labelStyle(.iconOnly)
             .fixedSize()
+#else
+        return labelStyle(.iconOnly)
+#endif
     }
 }
